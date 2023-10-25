ae3cd57b3c8f7e0677c1cf31266d0b0832794d5a
Liaksej <tarusov.liaksej@gmail.com>
2023-10-25 13:44:42 +0200

    Add new "deploy" script and necessary package
    Commit includes addition of a new command in package.json to enable easier deployment and the 'gh-pages' package to carry out the deployment. Minor refactoring of import statements and a change in the router basename in main.tsx was also done. The deployments are now automatic using GitHub pages. The refactoring and change were done to ensure best practices are followed in code structure. This commit will enable faster and more efficient deployment of application updates.

--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "@/routes/Home";
-import NewPost from "@/routes/NewPost.tsx";
-import Post from "@/routes/Post.tsx";
-import { loader as postsLoader } from "@/routes/PostsList.tsx";
-import { action as postsAction } from "@/routes/NewPost.tsx";
-import {
-  loader as postLoader,
-  action as deleteAction,
-} from "@/routes/Post.tsx";
-import EditPost, {
-  action as editAction,
-  loader as editLoader,
-} from "@/routes/EditPost.tsx";
-import * as process from "process";
+import NewPost, {action as postsAction} from "@/routes/NewPost.tsx";
+import Post, {action as deleteAction, loader as postLoader} from "@/routes/Post.tsx";
+import {loader as postsLoader} from "@/routes/PostsList.tsx";
+import EditPost, {action as editAction, loader as editLoader,} from "@/routes/EditPost.tsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    loader: postsLoader,
+    {
+      path: "/",
+      element: <Home/>,
+      loader: postsLoader,
+    },
+    {
+      path: "posts/new",
+      element: <NewPost/>,
+      action: postsAction,
+    },
+    {
+      path: "posts/:id",
+      element: <Post/>,
+      loader: postLoader,
+      action: deleteAction,
+    },
+    {
+      path: "posts/:id/edit",
+      element: <EditPost/>,
+      action: editAction,
+      loader: editLoader,
+    },
+  ], {
+    basename: process.env.NODE_ENV === "production" ? "/ra-react-router-crud-client" : "",
   },
-  {
-    path: "posts/new",
-    element: <NewPost />,
-    action: postsAction,
-  },
-  {
-    path: "posts/:id",
-    element: <Post />,
-    loader: postLoader,
-    action: deleteAction,
-  },
-  {
-    path: "posts/:id/edit",
-    element: <EditPost />,
-    action: editAction,
-    loader: editLoader,
-  },
-],{basename: process.env.NODE_ENV === "production" ? "/react-router-crud-client" : "/"});
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  base: process.env.NODE_ENV === "production" ? "/ra-react-router-crud-client/" : "",
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/ra-react-router-crud-client/" : "/",
 });
